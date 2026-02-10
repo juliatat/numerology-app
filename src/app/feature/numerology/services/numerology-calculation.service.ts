1361f699403acd3b73b09209cb9591cc4b3a3a5e
Julia <julia@Julia.local>
2026-02-10 11:00:13 +0100

    fix(numerology): correct Life Path Number calculation using arcana model

--- src/app/feature/numerology/services/numerology-calculation.service.ts
+++ src/app/feature/numerology/services/numerology-calculation.service.ts
@@ -1,23 +1,19 @@
-import {Injectable, inject} from '@angular/core';
+import {inject, Injectable} from '@angular/core';
 import {LifePathNumber} from '../../../core/models/numerology-types';
 import {ArcaneNumberService} from './arcane-number.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NumerologyCalculateService {
-
   private readonly arcaneService = inject(ArcaneNumberService);
 
   calculateLifePathNumber(date: Date): LifePathNumber {
-    const day = date.getDate();
+    const day = this.arcaneService.toArcane(date.getDate());
     const month = date.getMonth() + 1;
-    const year = date.getFullYear();
-    const sum =
-      this.arcaneService.sumDigits(day) +
-      this.arcaneService.sumDigits(month) +
-      this.arcaneService.sumDigits(year);
-
-    return this.arcaneService.toArcane(sum) as LifePathNumber;
+    const yearDigitsSum = this.arcaneService.sumDigits(date.getFullYear());
+    const year = this.arcaneService.toArcane(yearDigitsSum);
+    const total = day + month + year;
+    return this.arcaneService.toArcane(total) as LifePathNumber;
   }
 }
